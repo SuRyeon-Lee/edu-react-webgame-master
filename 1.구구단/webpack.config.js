d6ee5c132800e5808b05c768961d8f2695d870ae
SuRyeon-Lee <bunnyatgym@gmail.com>
2022-09-09 03:28:22 +0900

    :zap:  1.5.webpack + babel: 웹팩 설정 추가

--- 1.구구단/webpack.config.js
+++ 1.구구단/webpack.config.js
@@ -39,10 +39,11 @@
 
 
 
-//------------- 📝 My Homework -------------
-//lecture > webpack.config.js 참고해서 동일하게 작성해보기
+//------------- 📝 My Codes -------------
+//숙제 체점 + 추가
 
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   name: 'Gugudan-setting',
@@ -58,14 +59,36 @@ module.exports = {
 
   module: {
     rules:[{
-      test: /\.jsx/,
+      test: /\.jsx?$/,
       loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env','@babel/preset-react'],
+        // preset은 plugin의 모음
+        //'@babel/preset-env'안에도 수십게의 플러그인들이 합쳐져 있다.
+        // preset에도 설정을 적용할 수 있다.
+        // ['프리셋 이름', {설정}]
+        // 옛날 문법 너무 바꾸려면 babel 일이 많아져서 너무 느려진다.
+        presets: [
+          ['@babel/preset-env',{
+            targets: { 
+              //자동으로 옛날브라우저 지원하는데, 그걸 좀더 구체적으로 써줌
+              //예시로 아래와 같이 써주면 크롬의 최근 두개 버전만 호환된다.
+              //🌟개꿀-https://github.com/browserslist/browserslist
+              browsers: ['> 5% in KR'] //한국에서 5% 이상인 브라우저 모두 지원, 회사 요구사항 맞추기 아주 좋음
+            },
+            debug: true,
+          }],
+          '@babel/preset-react'
+        ],
+        plugins: [], //미리 많은 것을 다운받아 놓지말고, 오류가 나면 메세지 읽고 다운받든가 한다.
+        //debug: true, //💡
       }
     }]
   },
-
+  //모듈이나 rules외에 추가적으로 하고 싶으면 여기 plugin에 붙인다.
+  plugins: [
+    //위에서 설정한 loader에 options에 debug:trure넣어주도록 함 💡참고
+    new webpack.LoaderOptionsPlugin({debug:true}), 
+  ],
   output: {
     path: path.join(__dirname,'dist'),
     filename: 'app.js'
