cee1624eea254ba1931db3489a2af0cdfaba4e70
SuRyeon-Lee <bunnyatgym@gmail.com>
2022-09-12 17:38:35 +0900

    :sparkles: 2.끝말잇기: Hooks로 전환하기

--- 2.끝말잇기/WordRelay.jsx
+++ 2.끝말잇기/WordRelay.jsx
@@ -1,40 +1,40 @@
 const React = require('react');
-const { useState } = React;
+const { useState, useRef } = React;
 
 const WordRelay = () => {
-  const [word, setWord] = useState('제로초2');
-  const [value, setValue] = useState('');
-  const [result, setResult] = useState('');
-  const inputEl = React.useRef(null);
+  [word, setWord] = useState('제로초');
+  [value, setValue] = useState('');
+  [result, setResult] = useState('');
+  inputRef = useRef();
 
-  const onSubmitForm = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (word[word.length - 1] === value[0]) {
-      setResult('딩동댕');
-      setWord(value);
-      setValue('');
-      inputEl.current.focus();
-    } else {
-      setResult('땡');
-      setValue('');
-      inputEl.current.focus();
+    if (word[word.length - 1] === value[0]){
+      setResult('딩동댕')
+      setWord(value)
+      setValue('')
+      inputRef.current.focus();
+    }else{
+      setResult('땡')
+      setValue('')
+      inputRef.current.focus();
     }
-  };
+  }
+
+  const onChangeInput = (e) => {
+    setValue(e.target.value)
+  }
 
   return (
     <>
       <div>{word}</div>
-      <form onSubmit={onSubmitForm}>
-        <input
-          ref={inputEl}
-          value={value}
-          onChange={(e) => setValue(e.currentTarget.value)}
-        />
+      <form onSubmit={onSubmit}>
+        <input type="text" ref={inputRef} value={value} onChange={onChangeInput}/>
         <button>입력!</button>
       </form>
       <div>{result}</div>
     </>
-  );
-};
+  )
+}
 
-module.exports = WordRelay;
+module.exports = WordRelay
